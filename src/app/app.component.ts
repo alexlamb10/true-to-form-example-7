f9c383cc7dc788311651a97304fee264392f9d07
alexlamb10 <99216731+alexlamb10@users.noreply.github.com>
2023-06-13 15:27:46 -0600

    set up the outline, will look at the rest later when I have more info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { map, startWith } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {}
+export class AppComponent {
+  public form = this._fb.group({
+    subscribe: [false, [Validators.required]],
+  });
+
+  constructor(private _fb: FormBuilder) {}
+  public formUpdates$ = this.form.valueChanges.pipe(
+    startWith(this.form.value),
+    map((formValues) => {
+      return { ...formValues, valid: this.form.valid };
+    })
+  );
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CustomCheckboxComponent } from './custom-checkbox/custom-checkbox.component';
 
 @NgModule({
   imports: [BrowserModule, ReactiveFormsModule],
-  declarations: [AppComponent],
+  declarations: [AppComponent, CustomCheckboxComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
